702bc32080338d701f8b2e87f306430b9d7e0fed
Jaswal <ojaswi.jaswal@accenture.com>
2026-06-25 06:35:52 +0530

    vvvv

--- hello.py
+++ hello.py
@@ -2,4 +2,11 @@ print("HHH")
 print
     print("HHH")
 hhhh
+<<<<<<< HEAD
 dddd
+=======
+ddfgfdsa
+
+    print("oj")
+    print("oj")
+>>>>>>> 97f75d770afbc7228b8a1a154a9c9c8462759b07
---
+++ me.py
@@ -0,0 +1,15 @@
+password = "admin123"
+secret_key = "my-secret-key"
+
+
+
+undefined_variable
+
+def login(user):
+    if user == "admin":
+        print("Welcome")
+
+eval(input("Enter command: "))
+
+import subprocess
+subprocess.run("ls", shell=True)
